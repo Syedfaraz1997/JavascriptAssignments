634e8e18131659aeb605f949203cfb785c5a7dc6
Syedfaraz1997 <57364687+Syedfaraz1997@users.noreply.github.com>
2020-06-13 17:14:02 +0500

    Add files via upload

---
+++ Chapter02.js
@@ -0,0 +1,58 @@
+//Qno-1))
+
+    var username = "Syed Faraz Ali";
+    console.log("username" , username);
+
+//Qno-02)
+
+    var myname = "Syed Muhammad Faraz Ali";
+    console.log("myname" , myname);
+
+
+//Qno-03)
+
+    var message = "Hello World";
+    alert("Message " + message) ;
+
+//Qno-04)
+
+    var Stu_Name = " Syed Muhammad Faraz Ali";
+    var Stu_Age = 21;
+    var Stu_Course = "Web Mobiling And Developing";
+
+    alert( "Student Name " + Stu_Name);
+    alert("Student Age " +  Stu_Age);
+    alert( "Student Course  " +  Stu_Course);
+
+//Qno-05
+
+
+    var Pizza = "Pizza \nPizz \nPizz \nPiz \nPi \nP";
+
+    alert(Pizza);
+
+
+//Qno-06)
+
+      var E_mail = "syedfaraz210@gmail.com";
+      alert("My Email Address is " + E_mail);
+
+
+
+//Qno-07)
+
+    var Book = "A smarter way to learn JavaScript.";
+    alert("I am Tarying to learn From " + Book);
+
+//Qno-8)
+
+    var Home_Page = "<h2>Yeh! I can write HTML content trough Javascript</h2>";
+    document.write(Home_Page);
+
+
+//Qno-9)
+
+    var Space_write = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+    alert(Space_write);
+
+
