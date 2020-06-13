cf7d6671dc6aa269119c0a06fb42ba21b67ab4be
Syedfaraz1997 <57364687+Syedfaraz1997@users.noreply.github.com>
2020-06-13 17:15:40 +0500

    Add files via upload

---
+++ Chapter03.js
@@ -0,0 +1,34 @@
+// Qno-01))
+
+var Age_Number = 21;
+
+alert("I am " + Age_Number + "  Years Old");
+
+
+//Qn0-02))
+
+ 
+var N = 0;
+var Num = ++N; 
+alert("You have visited this site " + Num + " times");
+
+
+//Qno03))
+
+var birthyear = 1997;
+document.write("My birth year is  " + birthyear + " <br /> Data type of my declared variable is number <br />" );
+
+//Qno 04))
+
+    var visiter_Name = "Syed Muhammad Faraz Ali";
+    var Pro_Title = "Small Size T-Shirt ";
+    var Quantity = "Quantity is 5";
+
+    document.write("<h4>Name: " + visiter_Name + " Ordered by " + Pro_Title + " Quantity: " + Quantity
+     + "  on XYZ Clothing store</h4>" );
+
+
+
+//Qno-05))
+
+
