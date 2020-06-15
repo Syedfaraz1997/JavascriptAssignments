d1a89cc2d67ef85fefd1d1e2aef8165cf230818d
Syedfaraz1997 <57364687+Syedfaraz1997@users.noreply.github.com>
2020-06-15 17:20:24 +0500

    Add files via upload

---
+++ Chapter04.js
@@ -0,0 +1,81 @@
+//Qno-01))
+
+var First_One = "SYED Muhammad Faraz Ali";
+var Second_One = "Web-Developer";
+var Third_One = "syedfaraz210@gmail.com";
+
+
+document.write("<h4>I am " + First_One + " I am a " + Second_One + " Contact us " + Third_One + "</h4>");
+
+
+//Qno-02))
+
+    //  *************** Illegal Variable ********** //
+
+
+    //var first One = "faraz";      Space 
+    //var 89 = "faraz";      initailization is not be a Number 
+    // var first-name = "Syed";   
+    // var %name = "Muhammad";
+    // alert($name);
+
+    // var name =;
+
+        //  *************** legal Variable ********** //
+
+        //var name = "Faraz";
+        //var Middle_name = "Muhammad";
+        //var a = 34;
+        //var $name = "Syed ";
+       // var _ = "Ali";
+
+
+
+
+//Qno-03))
+
+
+// A)
+    var heading = "Rules for naming JS variables";
+    document.write("<h1>" + heading + "</h1>  <br/> <br> <br/>");
+
+
+// B)
+    var a = "number";
+    var b = "$";
+    var c = "_";
+
+    document.write("Variable names can only contain ," + a + " , " + b + " , " + c + " . For example $my_1stVariable");
+
+
+// C)
+
+    var d = "letter";
+    var e = "$";
+    var f = "_";
+
+    document.write("<br />Variables must begin with a " + d + " , " + e + " or " + f  + " . For example $name, _name or name");
+
+
+// D)
+
+     var sen = "sensitive";
+
+
+     document.write("<br> Variable names are case " + sen + " .");
+
+// E)
+
+    var key = "Keywords";
+
+
+
+    document.write("<br> Variable names should not be JS " + key + " .");
+
+
+
+
+
+
+
+
---
+++ Chapter05.js
@@ -0,0 +1,290 @@
+/*****------- Chapter no 05 --------*******/
+
+
+//Qno-1)
+    
+    var a = 3;
+    var b = 5;
+    var sum = a+b;
+
+    document.write("Sum of 3 and 5 is " + sum + " <br>");
+
+
+
+//Qno-02)
+
+      var a = 3;
+      var b = 5;
+      var sub = a-b;
+
+      document.write("Subtraction of 3 and 5 is " + sub + " <br>");
+
+
+
+      var a = 3;
+      var b = 5;
+      var mul = a*b;
+
+      document.write("Multiplication of 3 and 5 is " + mul + " <br>");
+
+
+      var a = 3;
+      var b = 5;
+      var div = a/b;
+
+      document.write("Subtraction of 3 and 5 is " + div + " <br>");
+
+
+      var a = 3;
+      var b = 5;
+      var mud = a%b;
+
+      document.write("Mudulus  of 3 and 5 is " + mud + " <br>");
+
+
+
+
+//Qno-03)
+
+    document.write("<br> Qno-03) <br>" + "<br> <br>")
+    var show = "decleare_variable";
+    var decleare_variable = 5;
+
+    document.write("Value after variable decleration is: " + show + " <br>");
+
+    document.write("<br> Initial value :" + decleare_variable);
+
+    var increament = ++decleare_variable;
+
+    document.write("<br> Value after Increament :" + increament);
+
+    var addition = 7;
+    var after_Addition = addition +increament;
+
+    document.write("<br> Value after Addition :" + after_Addition);
+
+    var Decreament = --after_Addition;
+
+
+    document.write("<br> Value after Decreament :" + Decreament );
+
+    var remainder = Decreament%3;
+
+    document.write("<br> This Remender :" + remainder + "<br> <br>");
+
+
+
+//Qno-04))
+
+    document.write("<br> Qno-04) <br>" + "<br> <br>");
+    var cost = 600;
+    var total = cost*5;
+
+
+    document.write("<br> Total cost to buy 5 ticks to a Movie is :" +  total + "PKR <br> <br>");
+
+
+//Qno -05))
+
+    
+    document.write("<br> Qno-05) <br>" + "<br> <br>")
+
+    for(i = 1;  i <= 10; i++){
+        document.write("4 * " + i + "  =  " + 4*i + "<br>");
+
+    }
+
+
+//Qno -06))
+
+    //******Fahrenheit  Tamperature **********//
+
+    document.write("<br> <br> Qno-06) <br>" + "<br> <br>");
+
+
+    var input = 70;
+    var  cel01 =  (input - 32);
+    var cel02 = 0.5555555555555556;
+
+    var celsius = cel01 * cel02;
+    document.write("<h4> 70 *F is  " + celsius + " *C  <br>  <br > </h4>")
+
+
+    
+     //******Celsius Tamperature **********//
+
+
+     var int = 25;
+     var fahren01 = int * 1.8;
+     var fahren02 = fahren01 + 32;
+
+     document.write("<h4>25 *C is  " + fahren02 + " *F <br ></h4>");
+
+
+
+     //Qno-07))
+
+     document.write("<br> <br> Qno-07) <br>" + "<br> <br>");
+
+
+      var item01 = 650;
+      var item02 = 100;
+
+      var quantity_item01 = 3*item01;
+
+      var quantity_item02 = 7*item02;
+
+      var shipping_charges = 100;
+
+      var tatal = quantity_item01 + quantity_item02 + shipping_charges;
+
+
+
+      document.write("Price of item01 is " + item01 + "<br>");
+
+      document.write("Quantity of item01 is 3 <br>");
+
+      document.write("Price of item02 is " + item02 + "<br>");
+
+      document.write("Quantity of item02 is 7 <br>");
+
+      document.write("Shipping Charges :" + shipping_charges);
+
+
+
+      document.write("Total cost of your Order :" + tatal + "<br> <br>");
+
+
+
+
+
+//Qno--08))
+
+document.write("<br> <br> Qno-08) <br>" + "<br> <br>");
+
+var math = 170;
+var physics = 175;
+var computer = 165;
+var chemistry = 170;
+var biology = 150;
+
+var tat = math + physics + computer + chemistry + biology;
+var tat_marks = 1000;
+var hundred = 100;
+//  var division = tal/tat_marks;
+var per = 830/1000*100 ;
+
+
+    document.write("Tatal Marks : " + tat_marks + "<br>");
+
+    document.write("Marks Obtained : " + tat + "<br>" );
+
+    document.write("Tatal Marks : " + per + "  % <br> <br>"  );
+
+
+
+
+//Qno-09)
+
+
+document.write("<br> <br> Qno-09) <br>" + "<br> <br>");
+
+var US_doller = 104.80;
+var Riyal = 28;
+
+var pkr_riyal = Riyal*25;
+var pkr_us = US_doller*10;
+
+var subm = pkr_riyal + pkr_us;
+
+document.write("Tatal Currency in PKR :" + subm + "<br> <br> <br>");
+
+
+
+//Qno-10))
+
+
+document.write("<br> <br> Qno-10) <br>" + "<br> <br>");
+
+    var nummber = 10;
+    
+    var Addition10 = nummber + 5;
+
+    var Multiplication10 = nummber * 10;
+
+    var  Division10 = nummber/5;
+
+
+    document.write("Add by 5 :" + Addition10 + " <br>  Multiply by 10 : " + Multiplication10 +  " <br> Division result by 2 :" + Division10 + "<br> <br>") ;
+
+    
+
+//Qno-11))
+
+
+document.write("<br> <br> Qno-11) <br>" + "<br> <br>");
+
+
+
+    var current_year = 2020;
+    var birth_years = 1998;
+
+    var age = current_year - birth_years;
+
+    document.write("Current years is :" + current_year + "<br> Birth years is " + birth_years + "<br> Your Age is " + age + "<br> <br>");
+
+
+//Qno-12))
+
+
+document.write("<br> <br> Qno-12) <br>" + "<br> <br>");
+
+     var radius_of_circle = 20;
+     var paye  = 3.143;
+    var formu = 2;
+ 
+     var circumference_of_circle = formu*paye*radius_of_circle;
+     var area_of_circle = paye*radius_of_circle**formu;
+
+ document.write("Radius of Circle : " + radius_of_circle +  "<br>  The circumference is : " + circumference_of_circle +
+                 "<br> The area of Circle : " + area_of_circle + "<br> <br> <br>");
+
+
+
+
+    
+
+
+
+
+
+//Qno-13))
+
+    
+      document.write("<br> <br> Qno-13) <br>" + "<br> <br>");
+
+
+      var favorite_snack = "Choco_chips";
+
+      var Current_Age = 21;
+
+      var Maximan_Age = 65;
+
+      var amount_snake = 3;
+
+      document.write("favorite snack : " + favorite_snack + " <br> your current age : " + Current_Age + "<br> maximum age : "
+      + Maximan_Age + " <br> You will need 150 Chocolate chip to last your until the ripe old age 65");
+
+
+
+
+
+
+
+
+
+    
+ 
+    
+
+
