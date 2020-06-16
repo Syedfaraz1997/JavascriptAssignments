54eb2ee9a7c33a574861f45d64ec9fab95514f20
Syedfaraz1997 <57364687+Syedfaraz1997@users.noreply.github.com>
2020-06-16 09:05:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+/********      CHAPTER no 01 Javascript******** */
+
+
+
+// Qno 1)
+
+window.alert("Welcome to our Websites");
+
+
+// Qno 2)
+
+alert(" Error! Please enter a valid password");
+
+//Qno 3)
+
+
+alert("Welcome to JS Land \n Happy Codding");
+
+//Qno-4)
+
+
+alert("" , "Welcome to JS Land \n Happy Codding");
+
+
+
+/*var a = 12;
+var b = 5; 
+var c = a-b;
+var d = a+b;
+var f = a*b;
+var g =a/b;
+
+console.log( "Subtarction" , c);
+
+
+console.log( "Addition" , d);
+
+console.log( "Multiplication" , f);
+
+console.log(  "Division" , g);
+
+
+*/
