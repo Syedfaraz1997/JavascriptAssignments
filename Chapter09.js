91fbb4e9adfa099abf395fffa7874f341843c994
Syedfaraz1997 <57364687+Syedfaraz1997@users.noreply.github.com>
2020-06-18 13:22:01 +0500

    Add files via upload

---
+++ Chapter09.js
@@ -0,0 +1,414 @@
+//Qno-01))
+/*
+
+      var city = "Your City is ";
+
+        var city_name = prompt("Please Enter Your City Name : " , city);
+
+        if(city_name === "karachi"){
+                alert("Welcome to the city of light : ");
+            }
+        else if(city_name === "lahore"){
+            alert("Welcome to the city of Qalander : ");
+        }
+        else if(city_name === "islamabad"){
+            alert("Welcome to the Capital of Pakistan: ");
+        }
+        else{
+            alert("Welcome to the City: ");
+        }
+*/
+
+
+//Qno-02))
+/*
+      document.write("<br> <br> Qno-02) <br>" + "<br> <br>");
+
+      var gender = "Your Gender is";
+
+      var Select_Gender = prompt("Please Enter your Gener :" , gender);
+
+      if(Select_Gender === "Male"){
+        alert("Good Morning Sir !! :")        
+      }else{
+            alert("Good Morning ma'am !! :")
+      }
+
+    */
+
+
+//Qno-03))
+/*
+      document.write("<br> <br> Qno-03) <br>" + "<br> <br>");
+
+
+      var single = "Please enter one Color";
+
+      var trafic_singinal = prompt("Please Enter your trafic color : " , single);
+
+      if(trafic_singinal === "Red"){
+        alert("Must Stop !! :");        
+      }
+      else if(trafic_singinal === "Yellow"){
+            alert("Ready to move !! :");
+      }else{
+        alert("Nothing About it !! :");
+      }
+      */
+
+
+
+//Qno-04))
+/*
+
+      document.write("<br> <br> Qno-04) <br>" + "<br> <br>");
+
+      var fuel = "Ypur Fuel is ";
+
+      var Quantity_fuel = +prompt("Please Enter the volume of fuel e.g : value in Kg :" , fuel);
+
+      if( Quantity_fuel <= 0.25){
+         alert("Please refill the fuel in your car" );
+      }
+      else{
+        alert("Your fuel Quantity is Preject Enjoye your Drive :");
+      }
+*/
+
+
+//Qno-05))
+
+
+  //  document.write("<br> <br> Qno-05) <br>" + "<br> <br>");
+
+    /******* Check the Code  A))**** */
+/*
+    var a = 4;
+    if (++a === 5){
+             alert("given condition for variable a is true");
+    }else{
+            alert("given condition for variable a is false");
+    }
+
+      // Ans is True
+
+      /******* Check the Code  B))**** */
+
+/*
+      var b = 82;
+        if (b++ === 83){
+        alert("given condition for variable b is true");
+        }
+        else{
+          alert("given condition for variable a is false");
+            }
+
+        //    Ans is False
+*/
+         /******* Check the Code  C))**** */
+
+ /*       var c = 12;
+          if (c++ === 13){
+          alert("condition 1 is true");
+          } 
+          else{
+            alert("given condition  1 false");
+          }
+
+            //Ans is False
+
+      
+
+          if (c === 13){
+          alert("condition 2 is true");
+          }else{
+                alert("condition 2 is false");
+          }
+
+          //Ans is true
+
+
+          if (++c < 14){
+            alert("condition 3 is true");
+            }
+          else{
+              alert("condition 3 is false");
+           }
+
+           // Ans is False
+
+           if(c === 14){
+            alert("condition 4 is true");
+            }
+            else{
+              alert("condition 4 is false");
+           }
+
+           //Ans is true 
+
+*/
+
+            
+    /******* Check the Code  D))**** */
+/*
+
+    var materialCost = 20000; 
+    var laborCost = 2000; 
+    var totalCost = materialCost + laborCost; 
+    
+    if (totalCost === laborCost + materialCost)
+    { 
+      alert("The cost equals"); 
+    }
+    else{
+      alert("The Cost is not Equal");
+    }
+
+*/
+    //Ans is True ;
+
+
+
+
+     /******* Check the Code  D))**** */
+
+/*
+     if (true)
+         { 
+         alert("True");
+         } 
+     if (false){
+        alert("False");
+       }
+
+       //1ST Condition only Run
+
+
+
+ */       
+     /******* Check the Code  D))**** */
+/*
+
+       f("car" < "cat")
+       { 
+         alert("car is smaller than cat");
+       }
+
+       // syntax Error two string not be greater & Less than to each other 
+
+*/
+
+
+
+//Qno-06))
+
+/*
+    document.write("<br> <br> Qno-06) <br>" + "<br> <br>");
+
+    math = "Your math Number is : ";
+    physics = "Your Physics Number is :";
+    Chemistry = "Your Chemistry Number is : ";
+
+    var math_num = +prompt("Please Enter your Math Number " , math);
+    var physics_num = +prompt("Please Enter your Physics  Number " , physics);
+    var Chemistry_num = +prompt("Please Enter your Math Number " , Chemistry);
+
+
+    var total_Obtain = math_num + physics_num + Chemistry_num;
+    var tital = 300;
+
+
+    var per = total_Obtain/tital *100;
+
+    document.write("<h1> Marks Sheet !</h1>" + "  <br><br>" ); 
+
+    document.write("<h4>Total Marks is " + tital + "<br>");
+    document.write("Obtain Marks is " + total_Obtain + "<br>");
+    document.write("Your Persentage is  " + per + " % <br></h4>");
+
+    if(per >= 80 )
+      {
+          document.write("<h4> Your Grade is A+ </h4>");
+          document.write("<h4> Remarks : Excellent Performance !</h4>");
+      }
+    else if( per >= 70 &  per <= 80)
+    {
+      document.write("<h4> Your Grade is A </h4>");
+      document.write("<h4> Remarks : Good Performance !</h4>");
+    }
+    else if( per >= 60 &  per <= 70)
+    {
+      document.write("<h4> Your Grade is B </h4>");
+      document.write("<h4> Remarks :  Improve your Performance !</h4>");
+    }
+    else if( per >= 50 &  per <= 60)
+    {
+      document.write("<h4> Your Grade is C </h4>");
+      document.write("<h4> Remarks :  Need to Hard work your Performance !</h4>");
+    }
+    else{
+      document.write("<h4> You are Failed </h4>");
+      document.write("<h4> Remarks :   Better Luck next time !</h4>");
+    }
+
+
+
+
+
+    */
+//Qno-07))
+/*
+
+      document.write("<br> <br> Qno-07) <br>" + "<br> <br>");   
+
+
+
+    var game = "Enter Number 1 TO 10 : ";
+
+    var int_num = +prompt("Please Enter Secret Number: " , game);
+    var b = int_num;
+    if(b === 5)
+      {
+        alert("Bengo!! You Win");
+      }
+    else if(b === b++)
+    {
+      alert("Your are so Close ! ");
+    }
+    else
+    {
+        alert("Good Luck for Next Time! ");
+    }
+*/
+
+//Qno-08))
+/*
+
+document.write("<br> <br> Qno-08) <br>" + "<br> <br>"); 
+
+ var inp = "Your Number is : ";
+
+ var user_num = +prompt("Please Enter an Number : " , inp);
+ var a  = user_num;
+
+    if(a%3 === 0 )
+   {
+        document.write("Your Number completely divided by Three: ");
+   }
+   else {
+    document.write("Your Number Not completely divided by Three: ");
+   }
+*/
+
+
+
+
+
+//Qno-09))
+/*
+
+document.write("<br> <br> Qno-09) <br>" + "<br> <br>");
+
+
+var inp = "Your Number is : ";
+
+var user_num = +prompt("Please Enter an Number : " , inp);
+var a  = user_num;
+
+   if(a%2 === 0 )
+  {
+       document.write("Your Number is Even : ");
+  }
+  else {
+   document.write("Your Number is Odd : ");
+  }
+
+  */
+
+
+
+
+
+//Qno-10))
+/*
+
+document.write("<br> <br> Qno-10) <br>" + "<br> <br>");
+  
+  var tem = "Today Tamperature ";
+  var today_tem = +prompt("Please Enter today Tamperature " , tem);
+  var b = today_tem;
+
+  if( b >= 40)
+    {
+        alert("It is too hot outside.”");
+    }
+  else if(b >= 30 & b <= 40)
+  {
+    alert("The Weather today is Normal.”");
+  }
+  else if(b >= 20 & b <= 30)
+  {
+    alert("The Weather today is Normal.”");
+  }
+  else if(b >= 10 & b <= 20)
+  {
+    alert("Today’s Weather is cool.”");
+  }
+  else if(b >= 0 & b <= 10)
+  {
+    alert("OMG! Today’s weather is so Cool.” ");
+  }
+  */
+
+
+
+
+
+//Qno-10))
+
+
+   document.write("<br> <br> Qno-10) <br>" + "<br> <br>");
+
+
+
+   var val1 = +prompt("Please Enter The Value One :" , "Your value is");
+   var operator =  prompt("Please Enter Operator :" , "Your Operator is");
+   var val2 = +prompt("Please Enter The Value Two :" , "Your value is");
+
+   if(operator === '+')
+    {
+        add = val1+val2;
+        alert("The Addition value of both is : " + add);
+    }
+   else if(operator === '-')
+   {
+    sub = val1-val2;
+    alert("The Subtraction value of both is : " + sub);
+   }
+   else if(operator === '*')
+   {
+    mul = val1*val2;
+    alert("The Multiplication  value of both is : " + mul);
+   }
+   else if(operator === '/')
+   {
+    div = val1/val2;
+    alert("The Division value of both is : " + div);
+   }
+   else if(operator === '%')
+   {
+    mod = val%val2;
+    alert("The Modulus value of both is : " + mod);
+   }
+   else{
+     alert("Something is Wrong !!!");
+   }
+
+
+
+
+
+
+
+
+
