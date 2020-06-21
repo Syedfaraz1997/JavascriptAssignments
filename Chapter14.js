69b280df47d48528159bbf084637dcc5d5d048a9
Syedfaraz1997 <57364687+Syedfaraz1997@users.noreply.github.com>
2020-06-21 22:26:46 +0500

    Add files via upload

---
+++ Chapter14.js
@@ -0,0 +1,244 @@
+//Qno-01))
+
+
+//     var stu_name = [];
+
+//     stu_name[0] = " Stu_Names : ";
+//     stu_name[1] = "Syed Faraz Ali,"
+
+//    console.log(stu_name );
+
+
+//Qno02))
+
+//    var student = {
+//     First_name : "Syed Muhammad",
+//     Last_name : "Faraz Ali",
+//     Roll_No : 12
+//     };
+
+//     console.log(student)
+
+
+//Qno-03))
+
+//    var str = ["sheraz" , "faizan" , "sameer" , "faraz" , "Azmeer" , "Abdulah " ];
+
+//    console.log(str);
+
+
+//Qno-04))
+
+//  var num =[20 , 2343 , 4345 , 534 , 2020 , 2 , 3435];
+
+//  console.log(num)
+
+
+//Qno-05))
+
+
+    // var  boo = ["false" , "true" , "XOR" , "OR" , "XNOR" ];
+    // console.log(boo);
+
+
+
+//Qno-06))
+
+
+
+
+    // var mix =  [ "syed" , "zaid" , 4320 , "false" , "kamil" , 320 , 4093];
+    // console.log(mix);
+
+
+//Qno-07))
+
+    // var Qualification = [ "Qualification" , "HCS","SSC","BS", "BCS", "MS", "M .Phill" ,"PHD"];
+    
+    // document.write("<h1>" + Qualification[0] + "<h1> <br>");
+    // document.write("<h4> 1)" + Qualification[1] + "<h4> <br>");
+    // document.write("<h4> 2)" + Qualification[2] + "<h4> <br>");
+    // document.write("<h4> 3)" + Qualification[3] + "<h4> <br>");
+    // document.write("<h4> 4)" + Qualification[4] + "<h4> <br>");
+    // document.write("<h4> 5)" + Qualification[5] + "<h4> <br>");
+    // document.write("<h4> 6)" + Qualification[6] + "<h4> <br>");
+    // document.write("<h4> 7)" + Qualification[7] + "<h4> <br>");
+
+
+//Qno08))
+
+    //     var stu_name = ["Sheraz" , "faraz" , "Faiz" ];
+
+    //     var stu_obtain = [320 , 230 , 480];
+
+    //     per01 = stu_obtain[0]/500 *100;
+    //     per02 = stu_obtain[1]/500 *100;
+    //     per03 = stu_obtain[2]/500 *100;
+
+
+    // document.write("<h3>Score of " + stu_name[0] + " is "+ stu_obtain[0] +" Persentage : " + per01 + " % </h3> <br>");
+    // document.write("<h3>Score of " + stu_name[1] + " is "+ stu_obtain[1] +" Persentage : " + per02 + " % </h3> <br>");
+    // document.write("<h3>Score of " + stu_name[2] + " is "+ stu_obtain[2] +" Persentage : " + per03 + " % </h3> <br>");
+
+
+
+//Qno09))
+
+    // document.write("<br> <br> <br> " + "<h4>Task A</h4>" + "<br>");
+
+    // //Task A:
+
+    // var color = ["Red" , "Green" , "Blue" , "Orange" , "White"];
+    // document.write(color + "<h4>  Initail List of Array </h4>");
+
+    // document.write("<br> <br>");
+
+    // var adds = prompt("Please Enter an color in the Beginning  of array :");
+    // color.unshift(adds);
+    // document.write( "<br>" + color + "<h4>    Add color At the Started of Array</h4>");
+  
+    //  //Task B:
+
+    //  document.write("<br> <br> <br> " + "<h4>Task B</h4>" + "<br>");
+    //  var adde = prompt("Please Enter an color in the End  of array :")
+    //  color.push(adde)
+    //  document.write( "<br>" +color + "<h4>        Add color At the Ended  of Array</h4>");
+
+    //  //Task C
+
+    //  document.write("<br> <br> <br> " + "<h4>Task C</h4>" + "<br>");
+
+    //  color.unshift("black" , "Yellow");
+    //  document.write( "<br>" +color + "<h4>        Add Two more color at start of array</h4>");
+
+
+
+     
+    //  //Task D
+
+    //  document.write("<br> <br> <br> " + "<h4>Task D</h4>" + "<br>");
+    //  color.shift();
+    //  document.write( "<br>" +color + "<h4>        Deleted first color of array</h4>");
+     
+     
+     
+    //  //Task E
+
+    //  document.write("<br> <br> <br> " + "<h4>Task E</h4>" + "<br>");
+    //  color.pop();
+    //  document.write( "<br>" +color + "<h4>        Deleted last color of array</h4>");
+
+    //   //Task f
+
+    //   document.write("<br> <br> <br> " + "<h4>Task f</h4>" + "<br>");
+    //   var index = prompt("Please Select the you want to added color");
+    //   var input01 = prompt("Please chose the color you want to added");
+
+    //   color.splice(index , 0 , input01);
+    //   document.write( "<br>" +color + "<h4>        added color any index you want</h4>");
+
+
+    //   //Task g
+
+    //   document.write("<br> <br> <br> " + "<h4>Task G</h4>" + "<br>");
+
+    //   var index01 = prompt("please Enter Deleted index color Number");
+    //   var input02 = prompt("how many color you deleted");
+
+    //   color.splice(index01 , input02);
+
+    //   document.write( "<br>" +color + "<h4>        added color any index you want</h4>");
+
+
+
+
+
+    //Qno-10))
+
+    document.write("<br> <br> <br> " + "<h4>Qno-10))</h4>" + "<br>");
+
+    var stu_score = [320 , 230 , 480 , 120];
+
+    document.write("<br>" + stu_score + "          Before Shorting <br> <br>");
+    stu_score.sort();
+
+    document.write("<br>" + stu_score + "            After Shorting <br>" );
+
+
+      //Qno-11))
+
+    //   document.write("<br> <br> <br> " + "<h4>Qno-11))</h4>" + "<br>");
+
+    //   var cites = ["Karachi" , "Lahore" , "Islamabad" , "Quetta" , "Peshawar"];
+
+    //   document.write("<h2>Cites List :</h2> <br> " + cites + "<br>");
+
+    //   var sel_cites = cites.slice(2,4);
+
+    //   document.write("<h2>Selected Cites :</h2> <br> " + sel_cites + "<br>");
+
+
+    //    //Qno-12))
+
+    //    document.write("<br> <br> <br> " + "<h4>Qno-12))</h4>" + "<br>");
+        
+    //    var intput = ["This" , "is" , "My" , "Cat"];
+
+    //    document.write("<h2> Array :</h2>" + intput + "<br><br>");
+
+    //    var jo = intput.join("  ");
+    
+    //    document.write("<h2> String :</h2>" +jo + "<br><br>");
+
+
+
+
+    
+//        //Qno-13))
+
+//         document.write("<br> <br> <br> " + "<h4>Qno-13))</h4>" + "<br>");
+
+//         arr1 = [];
+
+//         arr1[0] = "Keyboard";
+//         arr1[1] = "Mouse";
+//         arr1[2] = "printer";
+//         arr1[3] = "Moniter" ;
+
+
+//         document.write("<h2>Devics</h2> <br>" + arr1 + "<br>");
+
+//         document.write( arr1[0] + " <br> Out : <br>" + arr1[1] + "<br> Out : <br> " + arr1[2] + "<br> Out : <br> " +  arr1[3] + "<br>");
+
+
+// //Qno-14))
+
+//       document.write("<br> <br> <br> " + "<h4>Qno-14))</h4>" + "<br>");
+
+//       arr1.reverse();
+
+//       document.write("<h2>Devics</h2> <br>" + arr1 + "<br>");
+
+//         document.write( arr1[0] + " <br> Out : <br>" + arr1[1] + "<br> Out : <br> " + arr1[2] + "<br> Out : <br> " +  arr1[3] + "<br>");
+
+
+
+
+// //Qno-15))
+        
+        document.write("<br> <br> <br> " + "<h4>Qno-15))</h4>" + "<br>");
+
+        var devices =  ["Apple " , "Samsung " , "Motorolla " , " Nokia " ,
+        "Sony" ," Haier" ,"Infinix" , "Qmobile "  ];
+
+        document.write("<h1>Manufacturers List</h1> <br/>");
+
+        document.write( "  <h3> 1)" + devices[0] + "</h3>");
+        document.write( "  <h3> 2)" + devices[1] + "</h3>");
+        document.write( "  <h3> 3)" + devices[2] + "</h3>");
+        document.write( "  <h3> 4)" + devices[3] + "</h3>");
+        document.write( "  <h3> 5)" + devices[4] + "</h3>");
+        document.write( "  <h3> 6)" + devices[5] + "</h3>");
+        document.write( "  <h3> 7)" + devices[6] + "</h3>");
+        document.write( "  <h3> 8)" + devices[7] + "</h3>");
+
