86d186028315280f4abfaf13036c5ea328d8a87a
Syed Muhammad Faraz Ali <57364687+Syedfaraz1997@users.noreply.github.com>
2020-07-23 22:51:39 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,43 +1,85 @@
-/********      CHAPTER no 01 Javascript******** */
+// Qno-01))
+
+// Part 1  
 
 
-
-// Qno 1)
-
-window.alert("Welcome to our Websites");
+//Gettimg element of id “main-content” and assigning them in a variable.
 
 
-// Qno 2)
-
-alert(" Error! Please enter a valid password");
-
-//Qno 3)
+ var content = document.childNodes[1].childNodes[2].childNodes[1].childNodes[5]
+ console.log(content)
 
 
-alert("Welcome to JS Land \n Happy Codding");
+ ///Part 2 : Displaying all child elements of main content
 
-//Qno-4)
+ var c = document.body.children[0].children[2]
+ console.log(c)
 
-
-alert("" , "Welcome to JS Land \n Happy Codding");
-
-
-
-/*var a = 12;
-var b = 5; 
-var c = a-b;
-var d = a+b;
-var f = a*b;
-var g =a/b;
-
-console.log( "Subtarction" , c);
+ ///Part 3 : Getting elements of class render and showing thier inner html
 
 
-console.log( "Addition" , d);
+var c = document.body.children[0].children[2]
+var x = c.getElementsByTagName("p")
+// var content = x[0].innerHTML;
+// console.log(content)
 
-console.log( "Multiplication" , f);
+for (var i = 0; i < x.length ; i++) {
+    var content = x[i].innerHTML;
+     document.write(content)
+}
 
-console.log(  "Division" , g);
+//Part 4 and 5 :  Filling input values using javascript
+
+var val = document.getElementsByTagName("input")[0].setAttribute("value" , "Syed   Faraz Ali")
+
+var val1 = document.getElementsByTagName("input")[1].setAttribute("value" , "Syed Ishtiaque Ali")
+var val1 = document.getElementsByTagName("input")[2].setAttribute("value" , "syedfaraz210@gmail.com")
 
 
-*/
+//*****************************************************************// 
+//Question # 2
+
+Q//uestion 2 Part # 1 : Nodetype of element havinf id form-content
+
+var type =   document.childNodes[1].childNodes[2].childNodes[1].childNodes[3].nodeType
+console.log(type)
+
+//Part #2 
+var type =   document.childNodes[1].childNodes[2].childNodes[1].childNodes[5].childNodes[3].nodeType
+console.log(type)
+
+
+//Part 3 : Update child node of element having id “lastName”.
+
+var newNode = document.createTextNode("information")
+var x = document.body.children[0].children[2].children[1]
+x.replaceChild(newNode, x.childNodes[0])
+console.log(x)
+
+
+//TASK 4 : Get First and last child of id “main-content”
+
+var main = document.childNodes[1].childNodes[2].childNodes[1].childNodes[5].firstChild
+var main1 = document.childNodes[1].childNodes[2].childNodes[1].childNodes[5].lastChild
+
+console.log(main1)
+
+// TASK 5 a  : Next sibling of elemet having ID = LastName
+ var x = document.childNodes[1].childNodes[2].childNodes[1].childNodes[5].childNodes[1].nextSibling
+ console.log(x)
+
+
+// TASK 5 b : Previous sibling of elemet having ID = LastName
+var y = document.childNodes[1].childNodes[2].childNodes[1].childNodes[5].childNodes[1].previousSibling
+
+console.log(y)
+
+
+ //TASK 6 : Getting Parent Node 
+
+
+ var x = document.childNodes[1].childNodes[2].childNodes[1].childNodes[3].lastChild.parentNode.nodeName
+
+ var s = document.childNodes[1].childNodes[2].childNodes[1].childNodes[3].lastChild.nodeType
+
+ console.log(s)
